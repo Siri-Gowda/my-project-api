a618be6df7ee9cff78ac3e7af2dcb5095342dce7
Siruu123 <112840838+Siruu123@users.noreply.github.com>
2022-09-12 17:30:02 +0530

    new project added

---
+++ src/main/java/com/springboot/MyProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectApiApplication.class, args);
+	}
+
+}
